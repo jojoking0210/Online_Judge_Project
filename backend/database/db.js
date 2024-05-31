069779ab87f2e5e1fa62bf828168b7dd96b52218
Pranav Sarate <jojoking0210@gmail.com>
2024-05-31 13:34:33 +0530

    added Home,Register,login,problem Page

--- backend/database/db.js
+++ backend/database/db.js
@@ -7,6 +7,7 @@ const DBConnection = async () => {
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
+        
     } catch (error) {
         console.log('Error while connecting with the database ', error.message);
     }
